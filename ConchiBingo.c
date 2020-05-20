c7be2973e0ed59d17f24b4707389db49208d1438
juanmi9 <60710130+juanmi9@users.noreply.github.com>
2020-05-20 02:29:06 +0200

    Update ConchiBingo.c

--- ConchiBingo.c
+++ ConchiBingo.c
@@ -1,4 +1,4 @@
-#include <stdio.h>   
+#include <stdio.h>  
 #include <string.h>
 #include <stdlib.h>
 #include <time.h> 
@@ -19,7 +19,7 @@ int main  () {
 	
 	//DECLARACION DE VARIABLES
 	FILE * fusuarios;  //Fichero para la partida
-	int i=0, j=-1, k, l, m, n, z, numero, nusuarios, repetido,contador_linea1=0,contador_linea2=0,contador_linea3=0;  //Variables para bucles
+	int i=0, j=-1, a,b,c,d,e,k, l,q, m, n, z, numero, nusuarios, repetido,contador_linea1=0,contador_linea2=0,contador_linea3=0;  //Variables para bucles
 	int contador=0;
 	int bombo[100];  //Numeros del bombo
 	float ingreso = 0;  //Cantidad de ingreso
@@ -176,8 +176,8 @@ int main  () {
 						
 						for (m=0; m<=k; m++) {  //Se leen todos los numeros hasta la Ãºltima columna
 							for (n=0; n<=l; n++) {  //Se leen todos los numeros hasta la Ãºltima columna
-							
-								if(carton[m][n] == numero) {  //Si alguno de los numeros estÃ¡ repetido se crea un nuevo numero hasta que no lo estÃ©
+								a=carton[m][n];
+								if(a == numero) {  //Si alguno de los numeros estÃ¡ repetido se crea un nuevo numero hasta que no lo estÃ©
 									repetido = 1;  //Suponemos que estÃ¡ repetido
 								}
 							}	
@@ -211,12 +211,59 @@ int main  () {
 				numero=(rand()%90)+1;  //Queremos sacar un numero aleatoriamente entre el 1 y el 90
 				repetido=0;  //Suponemos que no esta repetido 
 				
-				for(k=0;k<z;k++) {  //Comprobamos que no lo esta con los numeros salidos anteriormente
-					if(bombo[k] == numero) { 
+				for(q=0;q<z;q++) {  //Comprobamos que no lo esta con los numeros salidos anteriormente
+					if(bombo[q] == numero) { 
 						repetido=1;  //Si esta repetido, sale del bucle for
 						
 					}
-				}	
+				}
+				for (i=0; i<nusuarios; i++) {  //Recorres todos los usuarios
+	    			for (j=0; j<usuario[i].cartones; j++) {  //Recorres todos los cartones
+	    				for (k=0; k<NFIL; k++) {  //Recorrer todas las filas
+							for (l=0; l<NCOL; l++) {  //Recorrer todas las columnas
+								b=carton[k][l];
+								if(numero == b) {
+									carton[k][l]= 0;  //Si el numero del bombo coincide con el del carton, se sustituye por 0
+									contador++;
+							
+									if(contador==15){
+										printf("El usuario %s ha cantado BINGO\n", usuario[i].nickname);
+										usuario[i].dinero += (1/2)*dinero_partida;
+										break;
+									}
+									/*
+									c=carton[0][l];
+									d=carton[1][l];
+									e=carton[2][l];
+							
+									if (c!=((c>0)&&(c<0))) {  //Para comprobar si hay linea
+										contador_linea1++;
+									} 
+									else if (d!=((d>0)&&(d<0))){
+										contador_linea2++;
+									} 
+									else if(e!=((e>0)&&(e<0))) {
+										contador_linea3++; 
+									}
+									if (contador_linea1 == 5 || contador_linea2 == 5 || contador_linea3 == 5) {
+										printf("¡El usuario %s ha cantado linea!", usuario[i].nickname);
+										usuario[i].dinero += (1/10)*dinero_partida;
+									}
+									*/
+								}	
+							}
+							if (contador == 15) {
+								break;
+							}
+						}
+						if (contador == 15) {
+							break;
+						}
+					}
+					if (contador == 15) {
+						break;
+					}
+				}
 			} while(repetido>0);
 		
 	    bombo[z]=numero;
@@ -224,48 +271,9 @@ int main  () {
 	    printf("\t| %d |\t", numero);
 	    printf("\n\t----\t");
 	    system ("pause");
-	    
-	    for (i=0; i<nusuarios; i++) {  //Recorres todos los usuarios
-	    	for (j=0; j<usuario[i].cartones; j++) {  //Recorres todos los cartones
-	    		for (k=0; k<NFIL; k++) {  //Recorrer todas las filas
-					for (l=0; l<NCOL; l++) {  //Recorrer todas las columnas
-						if(numero == carton[k][l]) {
-							carton[k][l] = 0;  //Si el numero del bombo coincide con el del carton, se sustituye por 0
-							contador++;
-							
-							if(contador==15){
-								printf("El usuario %s ha cantado BINGO\n", usuario[i].nickname);
-								break;
-							}
-							
-							if (carton[0][l] == 0) {  //Para comprobar si hay linea
-								contador_linea1++;
-							} else if (carton[1][l] == 0) {
-								contador_linea2++;
-							} else if (carton[2][l] == 0) {
-								contador_linea3++; 
-							}
-							if (contador_linea1 == 5 || contador_linea2 == 5 || contador_linea3 == 5) {
-								printf("¡El usuario %s ha cantado linea!", usuario[i].nickname);
-								usuario[i].dinero += (1/10)*dinero_partida;
-							}
-							if (contador_linea1 == 5 && contador_linea2 == 5 && contador_linea3 == 5) {
-								printf("¡El usuario %s ha cantado bingo!", usuario[i].nickname);
-								usuario[i].dinero += (1/2)*dinero_partida;
-							} 
-						if (contador == 15) {
-							break;	
-						}	
-					if (contador == 15)
-						break;}
-				if (contador == 15)
-					break;}
-			if (contador == 15)
-				break;}}
-		if (contador == 15)
-			break;}
-	if (contador == 15)
-	break;
+		if (contador == 15) {
+			break;
+		}
 	}
 	//usuario[bingo].dinero = usuario[bingo].dinero + (1/2)*dinero_partida;
 	//usuario[linea].dinero = usuario[linea].dinero + (1/10)*dinero_partida;
@@ -281,7 +289,6 @@ int main  () {
 	//Cerramos el fichero
 	fclose (fusuarios);
 }
-
 void titulo() {
 	printf("\n");
 	printf("+------   +------+   +        +   +------  +      +    -----    +-----+     -----   +        +   +-------   +------+\n");
